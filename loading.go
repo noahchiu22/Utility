a924f2b32cee84434e48eddd19febd6b30b7dfca
Noah <fg5678910@yahoo.com.tw>
2024-10-16 11:09:37 +0800

    loading bar mold

---
+++ loading.go
@@ -0,0 +1,25 @@
+package util
+
+func LoadingBar() {
+	// ------------------------------------讀取條------------------------------------
+	// db select all
+
+	// countNow = count
+	// for {
+	// fmt.Println(countNow, "/", count, int(float64(count-countNow)/float64(count)*100), "%")
+	// for i := 0; i < 100; i++ {
+	// 	if i < int(float64(count-countNow)/float64(count)*100) {
+	// 		print("*")
+	// 		continue
+	// 	}
+
+	// 	print("-")
+	// }
+
+	// db select all
+
+	// cmd := exec.Command("cmd", "/c", "cls")
+	// cmd.Stdout = os.Stdout
+	// cmd.Run()
+	// }
+}
