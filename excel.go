f3e98ad245d200e5f7321f6ff0e388c9fa3cc709
Noah <fg5678910@yahoo.com.tw>
2024-11-08 16:53:53 +0800

    feat CreateExcel AutofitColumn function

--- excel.go
+++ excel.go
@@ -1,12 +1,36 @@
 package util
 
 import (
+	"fmt"
 	"reflect"
+	"time"
+	"unicode"
+	"unicode/utf8"
 
 	"github.com/xuri/excelize/v2"
 )
 
-func CreateExcel[S comparable](data []S, path, filename string, headers ...string) (err error) {
+// Using any excel tag as title in the struct to create excel
+//
+//	type example struct {
+//		fieldName  any  `excel:"your title"`
+//	}
+//
+// Excel will be:
+//
+//	---------------
+//	|  title  | ...
+//	|---------+----
+//	| data[i] | ...
+//	|---------+----
+//	|data[i+1]| ...
+//	|---------+----
+//	|data[i+2]| ...
+//	|---------+----
+//	|    :    | ...
+//
+// And it will automatically fit column width by the cell content
+func CreateExcel[S comparable](data []S, path, filename string) (err error) {
 	f := excelize.NewFile()
 
 	defer func() {
@@ -15,34 +39,81 @@ func CreateExcel[S comparable](data []S, path, filename string, headers ...strin
 		}
 	}()
 
-	for col, header := range headers {
-		cell, _ := excelize.CoordinatesToCellName(col+1, 1)
-		f.SetCellValue("Sheet1", cell, header)
-	}
-
+	headers := []string{}
 	for row, item := range data {
-		rft := reflect.ValueOf(item)
+		t := reflect.TypeOf(item)
+		// 第一筆資料依照struct的tag excel製作headers
+		if row == 0 {
+			for i := 0; i < t.NumField(); i++ {
+				excelTag := t.Field(i).Tag.Get("excel")
+				if excelTag != "" {
+					headers = append(headers, excelTag)
+				}
+			}
+
+			for col, header := range headers {
+				cell, _ := excelize.CoordinatesToCellName(col+1, 1)
+				f.SetCellValue("Sheet1", cell, header)
+			}
+		}
+		v := reflect.ValueOf(item)
 		for col := 0; col < len(headers); col++ {
 			cell, _ := excelize.CoordinatesToCellName(col+1, row+2)
-			field := rft.Field(col)
+			field := v.Field(col)
+			excelTag := t.Field(col).Tag.Get("excel")
+			// 沒有excelTag就跳過
+			if excelTag == "" {
+				continue
+			}
 
-			var value interface{}
-			switch field.Type().String() {
-			case "int64":
-				value = field.Int()
-			case "float64":
-				value = field.Float()
+			value := field.Interface()
+			switch temp := value.(type) {
+			case int64:
+				value = temp
+			case float64:
+				value = temp
+			case time.Time:
+				value = temp.Format("2006-01-02 15:04:05")
 			default:
-				value = field.String()
+				value = temp
 			}
 			f.SetCellValue("Sheet1", cell, value)
 		}
 	}
 
+	AutofitColumn(f)
+
+	fmt.Println(path + filename)
 	// Save spreadsheet by the given path.
 	if err = f.SaveAs(path + filename); err != nil {
 		return
 	}
 
 	return
+}
+
+// Autofit all columns according to their text content
+func AutofitColumn(file *excelize.File) {
+	cols, _ := file.GetCols("Sheet1")
+	for i, col := range cols {
+		largestWidth := 0
+		for j, rowCell := range col {
+			fixedWidth := 0
+			for _, r := range rowCell {
+				if unicode.Is(unicode.Han, r) {
+					fixedWidth++
+				}
+			}
+			cellWidth := utf8.RuneCountInString(rowCell) + 4 + fixedWidth // for margin
+			if cellWidth > largestWidth {
+				largestWidth = cellWidth
+			}
+			file.SetRowHeight("Sheet1", j+1, 25)
+		}
+		name, err := excelize.ColumnNumberToName(i + 1)
+		if err != nil {
+			fmt.Println("err", err)
+		}
+		file.SetColWidth("Sheet1", name, name, float64(largestWidth))
+	}
 }
